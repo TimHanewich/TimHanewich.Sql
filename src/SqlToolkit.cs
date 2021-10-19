8b83560aa3ff70e457aeb86b07b27ce3d596f93f
Tim Hanewich <tahanewich@live.com>
2021-10-18 20:16:19 -0400

    Added SQL toolkit with ability to convert date times

---
+++ src/SqlToolkit.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace TimHanewich.SqlHelper
+{
+    public static class SqlToolkit
+    {
+        public static string ToSqlDateTimeString(this DateTime dt)
+        {
+            string ToReturn = "";
+            ToReturn = dt.Year.ToString("0000") + dt.Month.ToString("00") + dt.Day.ToString("00");
+            ToReturn = ToReturn + " " + dt.Hour.ToString() + ":" + dt.Minute.ToString();
+            return ToReturn;
+        }
+
+        public static string ToSqlDateString(this DateTime dt)
+        {
+            string ToReturn = "";
+            ToReturn = dt.Year.ToString("0000") + dt.Month.ToString("00") + dt.Day.ToString("00");
+            return ToReturn;
+        }
+    }
+}
