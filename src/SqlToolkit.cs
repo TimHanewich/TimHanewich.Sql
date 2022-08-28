e455e529313bf25b58bf604c4e22f28d08336944
Tim Hanewich <tahanewich@live.com>
2022-08-28 12:59:30 -0400

    Close data reader afte reading

--- src/SqlToolkit.cs
+++ src/SqlToolkit.cs
@@ -155,6 +155,7 @@ namespace TimHanewich.Sql
             {
                 await dr.ReadAsync();
                 ToReturn = dr.GetString(0);
+                await dr.CloseAsync();
             }
 
 
